a904d7085ae9240bedcb22e085090ddd81544be6
cforster <charlie@forsters.com>
2016-10-03 22:25:20 -0400

    Create SpaceMonger.java

---
+++ SpaceMonger.java
@@ -0,0 +1,18 @@
+/*
+ * #S1 Spacemonger (8 pts)
+ * 
+ * traverse a file system from a given point, and fill a rectangle with an image of the file system 
+ * with relative sizes, like the old program SpaceMonger: https://www.youtube.com/watch?v=7FNozMFKeU0
+ * 
+ */
+
+public class SpaceMonger {
+
+  public static void main(String[] args) {
+    space("/Users/student");
+  }
+
+  static void space(String url) {
+    //output a graphic
+  }
+}
